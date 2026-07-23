a42f1741b8e90ae5b13cad0748ed4b5ab1fb0965
PARTH SINGH <parthsingh1947@gmail.com>
2026-07-23 12:52:52 +0530

    Time: 930 ms (5.11%), Space: 23.8 MB (56.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
+         int n=nums.size();
+int i=0;
+int a=0;
+while(a<n-1)
+{
+for(int i=0;i<n-1;i++){
+if(nums[i]==0){
+int temp=nums[i];
+nums[i]=nums[i+1];
+nums[i+1]=temp;
+}
+}
+a++;
+}
     }
 };
