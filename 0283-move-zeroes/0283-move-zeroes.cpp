e3110f154adf2ac9e032475b752dd2943da91338
PARTH SINGH <parthsingh1947@gmail.com>
2026-08-14 14:04:48 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.8%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,34 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//          int n=nums.size();
+// int i=0;
+// int a=0;
+// while(a<n-1)
+// {
+// for(int i=0;i<n-1;i++){
+// if(nums[i]==0){
+// int temp=nums[i];
+// nums[i]=nums[i+1];
+// nums[i+1]=temp;
+// }
+// }
+// a++;
+// }
+//     }
+// };
+
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-         int n=nums.size();
-int i=0;
-int a=0;
-while(a<n-1)
-{
-for(int i=0;i<n-1;i++){
-if(nums[i]==0){
-int temp=nums[i];
-nums[i]=nums[i+1];
-nums[i+1]=temp;
-}
-}
-a++;
-}
+        int j = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
     }
 };
