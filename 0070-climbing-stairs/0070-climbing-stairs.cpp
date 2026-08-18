4ca850474707008eebd5229130727d09048f94ab
PARTH SINGH <parthsingh1947@gmail.com>
2026-08-18 21:34:50 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -17,3 +17,15 @@ public:
         return second;
     }
 };
+
+// class Solution {
+// public:
+
+//      int climbStairs(int n) {
+//         if(n<=2){
+//             return n;
+//             }
+//         return climbStairs(n-1)+climbStairs(n-2);
+
+//  }
+// };
