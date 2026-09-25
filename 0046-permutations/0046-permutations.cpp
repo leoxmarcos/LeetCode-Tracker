998721d2bb469d1de5d5b38f8cd2ca307ffc5a40
PARTH SINGH <parthsingh1947@gmail.com>
2026-09-25 13:50:19 +0530

    Time: 0 ms (100%), Space: 10.4 MB (88.38%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,57 +1,45 @@
-// class Solution {
-// public:
-    
-//         int permut(int arr[],vector<vector<int>>& ans,vector<int>temp,vector<bool>visited){
-//             if(visited.size()==temp.size()){
-//                 ans.push_back(temp);
-//                 return;
-//             }
-//             for(int i=0;i<arr.size();i++){
-//                 if(visited[i]==0)
-//                 {
-//                     visited[i]==1;
-//                     temp.push_back(arr[i]);
-//                     permut(arr,ans,temp,visited);
-//                     visited[i]=0;
-//                     temp.pop_back();
-//                 }
-//             }
-//         }
-//         vector<vector<int>> permute(vector<int>& nums) {
-            
-//     }
-// };
+
 class Solution {
 public:
     
-    void permut(vector<int>& arr, vector<vector<int>>& ans,
-                vector<int> temp, vector<bool> visited) {
-        
-        if(temp.size() == arr.size()) {
-            ans.push_back(temp);
+    void permut(vector<int>& arr, vector<vector<int>>& ans, int index){
+        if(index==arr.size()){
+            ans.push_back(arr);
             return;
         }
-        
-        for(int i = 0; i < arr.size(); i++) {
-            if(visited[i] == 0) {
-                
-                visited[i] = 1;
-                temp.push_back(arr[i]);
-                
-                permut(arr, ans, temp, visited);
-                
-                visited[i] = 0;
-                temp.pop_back();
-            }
+        for(int i =index;i<arr.size();i++){
+            swap(arr[i],arr[index]);
+            permut(arr,ans,index+1);
+            swap(arr[i],arr[index]);
         }
+        
     }
+    //             vector<int> temp, vector<bool> visited) {
+        
+    //     if(temp.size() == arr.size()) {
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+        
+    //     for(int i = 0; i < arr.size(); i++) {
+    //         if(visited[i] == 0) {
+                
+    //             visited[i] = 1;
+    //             temp.push_back(arr[i]);
+                
+    //             permut(arr, ans, temp, visited);
+                
+    //             visited[i] = 0;
+    //             temp.pop_back();
+    //         }
+    //     }
+    // }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        vector<bool> visited(nums.size(), 0);
         
-        permut(nums, ans, temp, visited);
+        
+        permut(nums,ans,0);
         
         return ans;
     }
